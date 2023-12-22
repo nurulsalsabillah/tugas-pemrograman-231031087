16065194513f27f2a21f1efa7c21ed4a34459c1e
nurulsalsabillah <153783359+nurulsalsabillah@users.noreply.github.com>
2023-12-22 22:56:54 +0800

    Add files via upload

---
+++ 231031087-nurul salsabillah-tugas-9-C.py
@@ -0,0 +1,20 @@
+biodata = {'nama'  : 'Nurul Salsabillah',
+           'nim'   : 231031087,
+           'prodi' : 'Sistem Informasi C',
+           'umur'  : 18,
+           'hobi'  : 'Menyanyi',
+           'alamat': 'Jalan Sumur Jodoh',
+           'asal'  : 'Parepare',
+           'status': 'Mahasiswa Aktif Institut Teknologi B.J Habibie'
+           }
+
+print(f'''
+Nama Saya  : {biodata['nama']}
+Dengan NIM : {biodata['nim']}
+Dari Prodi : {biodata['prodi']}
+Umur Saya  : {18}
+Hobi Saya  : {biodata['hobi']}
+Alamat Saya: {biodata['alamat']}
+Asal Saya  : {biodata['asal']}
+Status     : {biodata['status']}
+''')
